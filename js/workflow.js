f9826d78d236bedafa596fcd3a8326820563aaa2
jevvii <jayveemarcelo09@gmail.com>
2026-07-03 19:51:28 +0800

    fix(operations): robustly handle existing class attributes on SVGs in makeToolbarIcon

--- js/workflow.js
+++ js/workflow.js
@@ -4,7 +4,13 @@ const FINANCIAL_ACTION_ICONS = {
   transmittal: `<svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"/></svg>`
 };
 
-const makeToolbarIcon = (svg) => svg.replace(/^<svg([^>]*)>/i, '<svg$1 class="btn-icon-start">');
+const makeToolbarIcon = (svg) => {
+  if (!svg) return '';
+  if (/class=["']/i.test(svg)) {
+    return svg.replace(/class=(["'])(.*?)\1/i, 'class=$1$2 btn-icon-start$1');
+  }
+  return svg.replace(/^<svg([^>]*)>/i, '<svg$1 class="btn-icon-start">');
+};
 
 const FINANCIAL_ACTION_CONFIGS = [
   {
